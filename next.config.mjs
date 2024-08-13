69d877f974ddff6d57cb9e32e42aedfb72e2d204
Jamal Gregg <102499266+MaidenlessEldenLord@users.noreply.github.com>
2024-08-13 10:36:40 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true
+    images: {
+        remotePatterns: [
+            {hostname: "flowbite.s3.amazonaws.com"},
+        ]
+    },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
