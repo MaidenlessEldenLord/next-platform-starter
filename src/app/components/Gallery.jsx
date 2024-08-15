8d5e5a9e48ae4b6e43c8212f2899379aa15468ff
Jamal Gregg <102499266+MaidenlessEldenLord@users.noreply.github.com>
2024-08-14 23:41:44 -0400

    Update Gallery.jsx

--- src/app/components/Gallery.jsx
+++ src/app/components/Gallery.jsx
@@ -10,7 +10,7 @@ import treemapdiagram from "../../../public/treemapdiagram.jpg"
 import taskmate from "../../../public/taskmate.png"
 import notesapp from "../../../public/notesapp.png"
 import snowessence from "../../../public/snowessence.png"
-//import threadkount from "../../../public/threadkount"
+import threadkount from "../../../public/threadkount"
 import Link from 'next/link'
 
 const Gallery = () => {
@@ -133,6 +133,18 @@ const Gallery = () => {
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Rock Paper Scissors Shoot!</h5>
                     <Link href="https://replit.com/@Im2Shirts/Rock-Paper-ScissorsShoot" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Best Out Of 3
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+          <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <Image class="rounded-t-lg" src={threadkount} alt="Black Jack" />
+                <div class="p-5">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ThreadKount</h5>
+                    <Link href="" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Buy The Latest
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
